c36e473082e3e64a6eaf773c42b8c53feaebd0f1
BubbleFish <daforsastudia@gmail.com>
2025-03-26 22:39:02 +0300

    feat: Add a list of tags as constants

---
+++ mend/tags/tags.go
@@ -0,0 +1,17 @@
+package tags
+
+const TAG_INCLUDE = "include"
+const TAG_SLOT = "slot"
+const TAG_EXTEND = "extend"
+const TAG_RANGE = "range"
+const TAG_IF = "if"
+const TAG_UNLESS = "unless"
+
+var AllTags = []string{
+	TAG_INCLUDE,
+	TAG_SLOT,
+	TAG_EXTEND,
+	TAG_RANGE,
+	TAG_IF,
+	TAG_UNLESS,
+}
